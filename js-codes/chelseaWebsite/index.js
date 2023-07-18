61c756eea17351685a9ae899fdfe8d0d9eef49e6
AngadDogra <139108486+AngadDogra@users.noreply.github.com>
2023-07-17 23:37:36 -0700

    Add files via upload

---
+++ js-codes/chelseaWebsite/index.js
@@ -0,0 +1,13 @@
+
+let person = {
+  name: 'angad' , 
+  age: 18  
+} ;
+
+// console.log(person) ;
+
+// console.log(person.name) ;
+
+let selectedColours = ['red' , 'blue'] ;
+
+console.log(selectedColours[0]) ;
